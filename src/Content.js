295530ce40735d0ac57c22e1ea7ac87bd97389ae
Long Nhật <longle.24hdev@gmail.com>
2022-11-29 21:56:16 +0700

    useEffect

--- src/Content.js
+++ src/Content.js
@@ -1,5 +1,29 @@
+import { useEffect, useState } from "react";
+
 function Content() {
-  return <h1>Hello anh em</h1>;
+  const [title, setTitle] = useState("");
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then((res) => res.json())
+      .then((posts) => setPosts(posts));
+  });
+
+  return (
+    <div>
+      <input
+        type="text"
+        value={title}
+        onChange={(e) => setTitle(e.target.val)}
+      />
+      <ul>
+        {posts.map((post) => (
+          <li key={post.id}>{post.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default Content;
