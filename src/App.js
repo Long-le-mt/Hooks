6e901298b5ac047049e09d6dec1e1e25c2bf94dc
Long Nhật <longle.24hdev@gmail.com>
2022-11-29 18:09:14 +0700

    todo list

--- src/App.js
+++ src/App.js
@@ -2,83 +2,30 @@ import logo from "./logo.svg";
 import "./App.css";
 import { useState } from "react";
 
-const courses = [
-  {
-    id: 1,
-    name: "HTML, CSS",
-  },
-  {
-    id: 2,
-    name: "Javascript",
-  },
-  {
-    id: 3,
-    name: "ReactJS",
-  },
-];
-
 function App() {
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [checkedRadioId, setCheckedRadioId] = useState();
-  const [listCheckedCheckBoxId, setListCheckedCheckBoxId] = useState([]);
+  const [job, setJob] = useState("");
+  const [jobs, setJobs] = useState(() => {
+    return JSON.parse(localStorage.getItem("jobs")) || [];
+  });
 
-  const handleSubmit = () => {
-    console.log("submit", {
-      name,
-      email,
-      checkedRadioId,
-      listCheckedCheckBoxId,
-    });
-  };
+  const handleAddJob = () => {
+    const newJobs = [...jobs, job];
 
-  const handleCheck = (id) => {
-    const checked = listCheckedCheckBoxId.includes(id);
-    console.log("checked", checked);
-    if (checked) {
-      // uncheck
-      const newChecked = listCheckedCheckBoxId.filter((item) => item !== id);
-      setListCheckedCheckBoxId(newChecked);
-    } else {
-      setListCheckedCheckBoxId([...listCheckedCheckBoxId, id]);
-    }
+    // Save to local Storage
+    localStorage.setItem("jobs", JSON.stringify(newJobs));
+    setJobs([...jobs, job]);
+    setJob("");
   };
 
   return (
     <div className="App">
-      <input
-        type="text"
-        value={name}
-        onChange={(e) => setName(e.target.value)}
-      />
-      <input
-        type="text"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-      />
-
-      {courses.map((course) => (
-        <div key={course.id}>
-          <input
-            type="radio"
-            checked={checkedRadioId === course.id}
-            onChange={(e) => setCheckedRadioId(course.id)}
-          />
-          {course.name}
-        </div>
-      ))}
-
-      {courses.map((course) => (
-        <div key={course.id}>
-          <input
-            type="checkbox"
-            checked={listCheckedCheckBoxId.includes(course.id)}
-            onChange={() => handleCheck(course.id)}
-          />
-          {course.name}
-        </div>
-      ))}
-      <button onClick={handleSubmit}>Submit</button>
+      <input type="text" value={job} onChange={(e) => setJob(e.target.value)} />
+      <button onClick={handleAddJob}>Add job</button>
+      <ul>
+        {jobs.map((job, index) => (
+          <li key={index}>{job}</li>
+        ))}
+      </ul>
     </div>
   );
 }
