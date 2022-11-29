abf134f030374c4ee66270076b0958593b771a83
Long Nhật <longle.24hdev@gmail.com>
2022-11-29 22:35:22 +0700

    useEffect with dependicies

--- src/Content.js
+++ src/Content.js
@@ -1,14 +1,23 @@
 import { useEffect, useState } from "react";
 
+const tabs = ["posts", "comments", "albums"];
+
 function Content() {
   const [title, setTitle] = useState("");
   const [posts, setPosts] = useState([]);
+  const [typeTab, setTypeTab] = useState("");
 
   useEffect(() => {
     fetch("https://jsonplaceholder.typicode.com/posts")
       .then((res) => res.json())
       .then((posts) => setPosts(posts));
-  });
+  }, []);
+
+  useEffect(() => {
+    fetch(`https://jsonplaceholder.typicode.com/${typeTab}`)
+      .then((res) => res.json())
+      .then((posts) => setPosts(posts));
+  }, [typeTab]);
 
   return (
     <div>
@@ -17,9 +26,20 @@ function Content() {
         value={title}
         onChange={(e) => setTitle(e.target.val)}
       />
+      {tabs.map((tab) => (
+        <button
+          key={tab}
+          style={
+            typeTab === tab ? { color: "#fff", backgroundColor: "#333" } : {}
+          }
+          onClick={(e) => setTypeTab(tab)}
+        >
+          {tab}
+        </button>
+      ))}
       <ul>
         {posts.map((post) => (
-          <li key={post.id}>{post.title}</li>
+          <li key={post.id}>{post.title || post.name}</li>
         ))}
       </ul>
     </div>
