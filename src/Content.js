cb345d9db7018f6655f9d07ffa0284e709f47e9c
Long Nhật <longle.24hdev@gmail.com>
2022-11-30 17:51:13 +0700

    useRef

--- src/Content.js
+++ src/Content.js
@@ -1,29 +1,37 @@
-import { useEffect, useState } from "react";
-
-const tabs = ["posts", "comments", "albums"];
+import { useEffect, useRef, useState } from "react";
 
 function Content() {
-  const [count, setCount] = useState(1);
-  const [avatar, setAvatar] = useState("");
+  const [count, setCount] = useState(60);
+  const timerId = useRef();
+  const prevCount = useRef();
+  const h1Ref = useRef();
+
+  // lưu giá trị trước đó của state
+  useEffect(() => {
+    prevCount.current = count;
+  }, [count]);
 
   useEffect(() => {
-    return () => {
-      URL.revokeObjectURL(avatar.preview);
-    };
-  }, [avatar]);
+    console.log(h1Ref.current.getBoundingClientRect());
+  });
 
-  const handlePreviewAvatar = (e) => {
-    const file = e.target.files[0];
+  const handleStart = () => {
+    timerId.current = setInterval(() => {
+      setCount((prevCount) => prevCount - 1);
+    }, 1000);
+  };
 
-    file.preview = URL.createObjectURL(file);
-    console.log(URL.createObjectURL(file));
-    setAvatar(file);
+  const handleStop = () => {
+    clearInterval(timerId.current);
+
+    console.log("Stop ->", timerId.current);
   };
 
   return (
     <div>
-      <input type="file" onChange={handlePreviewAvatar} />
-      {avatar && <img src={avatar.preview} alt="" width="80%" />}
+      <h1 ref={h1Ref}>{count}</h1>
+      <button onClick={handleStart}>Start</button>
+      <button onClick={handleStop}>Stop</button>
     </div>
   );
 }
