57385cb79a9f81c7adfc6746144519e11ace22f4
Long Nhật <longle.24hdev@gmail.com>
2022-12-01 05:06:46 +0700

    useReducer basic

--- src/App.js
+++ src/App.js
@@ -1,66 +1,46 @@
-import { useMemo, useRef, useState } from "react";
+import { upload } from "@testing-library/user-event/dist/upload";
+import { useMemo, useReducer, useRef, useState } from "react";
 import "./App.css";
 
+// useState
+// 1. Init state: 0
+// 2. Actions: Up(state + 1) / Down(state - 1)
+
+// useReducer
+// Init state
+// Actions: Up(state + 1) / Down(state - 1)
+// Reducer
+// Dispatch
+
+// Init state
+const initState = 0;
+
+// Actions
+const UP_ACTION = "up";
+const DOWN_ACTION = "down";
+
+// Reducer
+// state là initState mà do ta khởi tạo
+const reducer = (state, action) => {
+  switch (action) {
+    case UP_ACTION:
+      return state + 1;
+    case DOWN_ACTION:
+      return state - 1;
+    default:
+      throw new Error("invalia action");
+  }
+};
+
 function App() {
-  const [name, setName] = useState("");
-  const [price, setPrice] = useState("");
-  const [products, setProducts] = useState([]);
-
-  const ref = useRef();
-
-  const handleSubmit = () => {
-    setProducts([
-      ...products,
-      {
-        name,
-        price: +price,
-      },
-    ]);
-
-    setName("");
-    setPrice("");
-    ref.current.focus();
-  };
-
-  // Nếu viết như này, total sẽ được tính toán lại khi re-render, total chỉ cần thực sự tính toán lại khi nó ds products thay đổi
-  // const total = products.reduce((result, prod) => {
-  //   console.log("Tính toán lại");
-  //   return result + prod;
-  // }, 0);
-
-  // Sử dụng memo để tránh thực hiện lại 1 đoạn logic không cần thiết
-  const total = useMemo(() => {
-    const result = products.reduce((result, prod) => {
-      console.log("Tính toán lại");
-      return result + prod.price;
-    }, 0);
-
-    return result;
-  }, [products]);
+  // Chạy lần đầu tiên nhưng hàm reducer chưa thực thi
+  const [count, dispatch] = useReducer(reducer, initState);
 
   return (
-    <div className="App">
-      <input
-        type="text"
-        value={name}
-        onChange={(e) => setName(e.target.value)}
-        ref={ref}
-      />
-      <input
-        type="text"
-        value={price}
-        onChange={(e) => setPrice(e.target.value)}
-      />
-      <br />
-      <button onClick={handleSubmit}>Add</button>
-      Total: {total}
-      <ul>
-        {products.map((product, index) => (
-          <li key={index}>
-            {product.name} - {product.price}
-          </li>
-        ))}
-      </ul>
+    <div>
+      <h1>{count}</h1>
+      <button onClick={() => dispatch(DOWN_ACTION)}>Down</button>
+      <button onClick={() => dispatch(UP_ACTION)}>Up</button>
     </div>
   );
 }
