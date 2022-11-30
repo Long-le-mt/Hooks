d434f5b035bd4a5a523e42010a4d9147e09eddc0
Long Nhật <longle.24hdev@gmail.com>
2022-12-01 04:48:45 +0700

    useMemo

--- src/App.js
+++ src/App.js
@@ -1,19 +1,66 @@
-import logo from "./logo.svg";
+import { useMemo, useRef, useState } from "react";
 import "./App.css";
-import { useCallback, useState } from "react";
-import Content from "./Content";
 
 function App() {
-  const [count, setCount] = useState(0);
-  const increase = useCallback(() => {
-    setCount(count + 1);
-  }, []);
+  const [name, setName] = useState("");
+  const [price, setPrice] = useState("");
+  const [products, setProducts] = useState([]);
+
+  const ref = useRef();
+
+  const handleSubmit = () => {
+    setProducts([
+      ...products,
+      {
+        name,
+        price: +price,
+      },
+    ]);
+
+    setName("");
+    setPrice("");
+    ref.current.focus();
+  };
+
+  // Nếu viết như này, total sẽ được tính toán lại khi re-render, total chỉ cần thực sự tính toán lại khi nó ds products thay đổi
+  // const total = products.reduce((result, prod) => {
+  //   console.log("Tính toán lại");
+  //   return result + prod;
+  // }, 0);
+
+  // Sử dụng memo để tránh thực hiện lại 1 đoạn logic không cần thiết
+  const total = useMemo(() => {
+    const result = products.reduce((result, prod) => {
+      console.log("Tính toán lại");
+      return result + prod.price;
+    }, 0);
+
+    return result;
+  }, [products]);
 
   return (
     <div className="App">
-      <Content onIncrease={increase} />
-      <h1>{count}</h1>
-      {/* <button onClick={increase}>Click me!</button> */}
+      <input
+        type="text"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+        ref={ref}
+      />
+      <input
+        type="text"
+        value={price}
+        onChange={(e) => setPrice(e.target.value)}
+      />
+      <br />
+      <button onClick={handleSubmit}>Add</button>
+      Total: {total}
+      <ul>
+        {products.map((product, index) => (
+          <li key={index}>
+            {product.name} - {product.price}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
